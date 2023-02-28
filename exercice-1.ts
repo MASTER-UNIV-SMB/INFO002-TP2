7337306ef00e055a3b6578c4f791fadf5a6a7b8f
PHClement <c.delouvencourt@nocturne.app>
2023-02-28 15:38:06 +0100

    ici ca commit

--- exercice-1.ts
+++ exercice-1.ts
@@ -22,7 +22,7 @@ function invertLine(img: Bitmap): Bitmap {
 function invertHalf(img: Bitmap): Bitmap {
     const m = Math.round(img.height / 2);
 
-    for (let j = 0; m < img.height; j++) {
+    for (let j = m; j < img.height; j++) {
         for (let i = 0; i < img.width; i++) {
             const pixelsBuffer: ArrayBuffer = img.getPixelRGBA_separate(i, j);
             const pixels = new Uint8Array(pixelsBuffer);
@@ -38,18 +38,135 @@ function invertHalf(img: Bitmap): Bitmap {
     return img;
 }
 
+function hideMessage(img: Bitmap, img2: Bitmap, message: string): {img: Bitmap, img2: Bitmap} {
+    if(img.width < 32) throw new Error("Image trop petite");
+
+    console.debug("Longueur envoyé : " + message);
+
+    const bytes = new Uint8Array(img.data);
+
+    for(let i = 0; i < 32; i++) {
+        const pixelsBuffer: ArrayBuffer = img.getPixelRGBA_separate(i, 0);
+        const pixels = new Uint8Array(pixelsBuffer);
+        const bytes_len = pixels.length;
+
+        let r = pixels[0];
+        let g = pixels[1];
+        let b = pixels[2];
+
+        r = r >> 1;
+        r = r << 1;
+        r = r ^ ((bytes_len >> i) & 1)
+
+        img.setPixelRGBA_i(i, 0, r, g, b, 255);
+        if (r !== (r ^ bytes.byteLength >> i & 1)) img2.setPixelRGBA_i(i, 0, 255, 0, 0, 255);
+    }
+
+
+    for(let i = 32; i < bytes.length * 8 + 32; i = i + 8) {
+        let temp = bytes[(i-32)/8];
+        console.debug(`${i}/${bytes.length * 8 + 32}`);
+        for(let j = 0; j < 8; j++) {
+            let y = Math.round((i + j) / img.width);
+            let x = Math.round((i + j) % img.width);
+
+            const pixelsBuffer: ArrayBuffer = img.getPixelRGBA_separate(x, y);
+            const pixels = new Uint8Array(pixelsBuffer);
+
+            let r = pixels[0];
+            let g = pixels[1];
+            let b = pixels[2];
+
+            r = r >> 1;
+            r = r << 1;
+            r = r ^ ((temp >> i) & 1)
+
+            img.setPixelRGBA_i(i, 0, r, g, b, 255);
+            if (r !== (r ^ bytes.byteLength >> j & 1)) img2.setPixelRGBA_i(i, 0, 255, 0, 0, 255);
+        }
+    }
+
+    return {img, img2};
+}
+
+function showMessage(img: Bitmap): string{
+    let bytes_len = 0;
+    let bytes: Uint8Array;
+
+    for (let i = 0; i < 32; i++) {
+        const pixelsBuffer: ArrayBuffer = img.getPixelRGBA_separate(i, 0);
+        const pixels = new Uint8Array(pixelsBuffer);
+
+        const r = pixels[0];
+
+        const a = (r & 1) << i;
+        bytes_len += a;
+    }
+
+    console.debug("Longueur reçu : " + bytes_len)
+
+    bytes = new Uint8Array(bytes_len);
+
+    for(let i = 32; i < bytes_len * 8 + 32; i = i + 8) {
+        let temp = 0;
+        console.debug(`${i}/${bytes_len * 8 + 32}`);
+        for(let j = 0; j < 8; j++) {
+            let y = Math.round((i + j) / img.width);
+            let x = Math.round((i + j) % img.width);
+
+            const pixelsBuffer: ArrayBuffer = img.getPixelRGBA_separate(x, y);
+            const pixels = new Uint8Array(pixelsBuffer);
+
+            let r = pixels[0];
+
+            const a = (r & 1) << j;
+            temp += a;
+        }
+        bytes[i/8] = temp;
+    }
+
+    return new TextDecoder().decode(bytes);
+}
+
 async function main() {
     const diplomeImage = await PImage.decodePNGFromStream(fs.createReadStream("assets/diplome-BG.png"));
+    const mamadouImage = await PImage.decodePNGFromStream(fs.createReadStream("assets/mamadou.png"));
 
-    // Exercice 1
+    console.debug ("Exercice : Inversion de la ligne du milieu...")
     const bitmap = invertLine(diplomeImage);
     const out = fs.createWriteStream("assets/diplome-BG-inverted.png");
     await PImage.encodePNGToStream(bitmap, out);
+    console.debug("Exercice 1 : OK")
 
-    // Exercice 2
+    console.debug("Exercice 1 : Inversion de la moitié de l'image...")
     const bitmap2 = invertHalf(diplomeImage);
     const out2 = fs.createWriteStream("assets/diplome-BG-half-inverted.png");
     await PImage.encodePNGToStream(bitmap2, out2);
+    console.debug("Exercice 1 : OK")
+
+    console.debug("Exercice 1 : Inversion de la moitié de l'image...")
+    const bitmap3 = invertHalf(mamadouImage);
+    const out3 = fs.createWriteStream("assets/mamadou-BG-half-inverted.png");
+    await PImage.encodePNGToStream(bitmap3, out3);
+    console.debug("Exercice 1 : OK")
+
+    console.debug("Exercice 1 : Cacher un élément dans une image...")
+    /*const {img, img2} = hideMessage(diplomeImage, mamadouImage, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\n" +
+        "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\n" +
+        "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n" +
+        "consequat.  Duis aute irure dolor in reprehenderit in voluptate velit esse\n" +
+        "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\n" +
+        "proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    const out4 = fs.createWriteStream("assets/diplome-message-cache.png");
+    await PImage.encodePNGToStream(img, out4);
+    const out5 = fs.createWriteStream("assets/diplome-message-cache-diff.png");
+    await PImage.encodePNGToStream(img2, out5);*/
+    console.debug("Exercice 1 : OK")
+
+    console.debug("Exercice 1 : Afficher un texte caché dans une image...");
+    const secretImage = await PImage.decodePNGFromStream(fs.createReadStream("assets/diplome-message-cache-diff.png"));
+    const text = showMessage(secretImage);
+    console.debug("Exercice 1 : Texte caché" + text)
 }
 
 main()
